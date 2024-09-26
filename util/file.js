b8bf06bdc2c8ff943de1094d043dc69455090797
Ankush <rodewad.ankush29@gmail.com>
2024-09-27 04:19:48 +0530

    bug fixes in filePath validation

--- util/file.js
+++ util/file.js
@@ -2,17 +2,16 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const isFileExist = async (fileName) => {
-    const filePath = path.join(process.cwd(), fileName);
-    console.log(filePath)
-    try {
-        await fs.access(filePath, fs.constants.F_OK);
-        console.log(`${fileName} exists.`);
-        return true;
-    } catch (err) {
-        console.log(`${fileName} does not exist.`);
-        return false;
-    }
-
+  const filePath = path.resolve(fileName);
+  console.log(`Resolved file path: ${filePath}`);
+  try {
+    await fs.access(filePath, fs.constants.F_OK);
+    console.log(`${fileName} exists.`);
+    return true;
+  } catch (err) {
+    console.log(`${fileName} does not exist.`);
+    return false;
+  }
 };
 
 module.exports = { isFileExist };
--- util/validation.js
+++ util/validation.js
@@ -3,7 +3,18 @@ const path = require('path');
 const isValidFilePath = (filePath) => {
   // Simple validation to check for forbidden characters or patterns
   const forbiddenPatterns = /[<>:"|?*]/;
-  return !forbiddenPatterns.test(filePath) && path.isAbsolute(filePath);
+
+  // Normalize the path to handle different path separators
+  const normalizedPath = path.normalize(filePath);
+
+  // Check for forbidden characters
+  if (forbiddenPatterns.test(normalizedPath)) {
+    return false;
+  }
+
+  // Additional checks can be added here if necessary
+
+  return true;
 };
 
 module.exports = { isValidFilePath };
