e2db84c7fe534d5aeed11eae3d0d9d67ceed4fc0
Ankush <rodewad.ankush29@gmail.com>
2024-09-27 05:46:09 +0530

    added stop and status commands

--- util/file.js
+++ util/file.js
@@ -3,13 +3,13 @@ const path = require("path");
 
 const isFileExist = async (fileName) => {
   const filePath = path.resolve(fileName);
-  console.log(`Resolved file path: ${filePath}`);
+  // console.log(`Resolved file path: ${filePath}`);
   try {
     await fs.access(filePath, fs.constants.F_OK);
-    console.log(`${fileName} exists.`);
+    // console.log(`${fileName} exists.`);
     return true;
   } catch (err) {
-    console.log(`${fileName} does not exist.`, err.message);
+    // console.log(`${fileName} does not exist.`, err.message);
     return false;
   }
 };
